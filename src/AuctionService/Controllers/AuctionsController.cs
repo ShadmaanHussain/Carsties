05286f254c1bd6769337ecb00485aeebce7adf27
Shadmaan Hussain <v-shadmaanh@microsoft.com>
2026-06-10 13:17:28 +0530

    created auction service

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,105 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers
+{
+    [Route("api/auctions")]
+    [ApiController]
+    public class AuctionsController : ControllerBase
+    {
+        private readonly AuctionDbContext _context;
+        private readonly IMapper _mapper;
+        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+        {
+            var auctions = await _context.Auctions
+                .Include(x => x.Item)
+                .OrderBy(x => x.Item.Make)
+                .ToListAsync();
+            return _mapper.Map<List<AuctionDto>>(auctions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+            var auction = await _context.Auctions
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (auction == null)
+            {
+                return NotFound();
+            }
+            return _mapper.Map<AuctionDto>(auction);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
+        {
+            var auction = _mapper.Map<Auction>(createAuctionDto);
+            auction.Seller = "Test"; // To do - Add current user as seller
+            _context.Auctions.Add(auction);
+            var results = await _context.SaveChangesAsync();
+            if (results <= 0)
+            {
+                return BadRequest("Failed to create auction, Could not save changes to the database.");
+            }
+            var auctionDto = _mapper.Map<AuctionDto>(auction);
+            return CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, auctionDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+        {
+            var auction = await _context.Auctions
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (auction == null)
+            {
+                return NotFound();
+            }
+            // To do - Seller name must match the current user to update the auction
+            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+            var results = await _context.SaveChangesAsync();
+
+            if (results <= 0)
+            {
+                return BadRequest("Failed to update auction, Could not save changes to the database.");
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAuction(Guid id)
+        {
+            var auction = await _context.Auctions.FindAsync(id);
+            if (auction == null)
+            {
+                return NotFound();
+            }
+            // To do - Seller name must match the current user to delete the auction
+            _context.Auctions.Remove(auction);
+            var results = await _context.SaveChangesAsync();
+            if (results <= 0)
+            {
+                return BadRequest("Failed to delete auction, Could not save changes to the database.");
+            }
+            return Ok();
+        }
+    }
+}
---
+++ src/AuctionService/DTOs/AuctionDto.cs
@@ -0,0 +1,22 @@
+﻿namespace AuctionService.DTOs
+{
+    public class AuctionDto
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime AuctionEnd { get; set; }
+        public string Seller { get; set; } = string.Empty;
+        public string Winner { get; set; } = string.Empty;
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public int Milage { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public int ReservePrice { get; set; }
+        public int? SoldAmount { get; set; }
+        public int? CurrentHighBid { get; set; }
+    }
+}
